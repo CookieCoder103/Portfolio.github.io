3f52670ee12789ab3d48b066585ccec3b1ed1ea9
Sam Yavari <126590851+CookieCoder103@users.noreply.github.com>
2024-04-09 23:42:31 +0100

    Tech stack carousel + Animations

--- script.js
+++ script.js
@@ -7,16 +7,39 @@ toggleSwitch.addEventListener("change", () => {
   body.classList.toggle("light");
 });
 
-
-// Text Carousel 
-const scroller = document.querySelectorAll(".scroller");
+// Tech Stack Carousel
+const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation()
+  addAnimation();
 }
 
 function addAnimation() {
-    scrollers.forEach((scroller) => {
-        scroller.setAttribute("data-animated", true);
-    })
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
 }
+
+// Animaiton on Scroll 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
